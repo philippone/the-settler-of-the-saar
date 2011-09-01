17bf8a21099d8dfd7f0205a07691c76d43feb2e4
Kevin Streit <streit@cs.uni-saarland.de>
2011-09-01 14:00:43 +0200

    Added main classes; Minor changes

---
+++ implementation/saarsiedler-groupXX/src/main/java/de/unisaarland/cs/sopra/ai/AiMain.java
@@ -0,0 +1,9 @@
+package de.unisaarland.cs.sopra.ai;
+
+public class AiMain {
+
+	public static void main(String[] args) {
+		System.err.println("This is the not yet implemented main method of the AI...");
+	}
+
+}
---
+++ implementation/saarsiedler-groupXX/src/main/java/de/unisaarland/cs/sopra/ui/UiMain.java
@@ -0,0 +1,9 @@
+package de.unisaarland.cs.sopra.ui;
+
+public class UiMain {
+
+	public static void main(String[] args) {
+		System.err.println("This is the not yet implemented main method of the UI...");
+	}
+
+}
