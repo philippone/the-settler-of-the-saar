71d72bddb79212b90e18e626b43e120ec76a924b
Valentin <valentins@online.de>
2011-09-05 20:54:11 +0200

    implenments modelobserver

--- implementation/saarsiedler-group16/src/main/java/de/unisaarland/cs/sopra/ai/AiMain.java
+++
@@ -1,9 +0,0 @@
-package de.unisaarland.cs.sopra.ai;
-
-public class AiMain {
-
-	public static void main(String[] args) {
-		System.err.println("This is the not yet implemented main method of the AI...");
-	}
-
-}
