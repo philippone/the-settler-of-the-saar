d86edd6d80b7938eba60c83efac60f93e521d3de
Sylvain <sylvain.carpe@infonie.fr>
2011-09-05 15:49:26 +0200

    Catapulttest

--- saarsiedler-group16/src/test/java/model/CatapultTest.java
+++ saarsiedler-group16/src/test/java/model/CatapultTest.java
@@ -4,8 +4,6 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 
-import de.unisaarland.cs.sopra.common.model.Location;
-import de.unisaarland.cs.sopra.common.model.Path;
 import de.unisaarland.cs.sopra.common.model.Player;
 import de.unisaarland.cs.sopra.common.model.Catapult;
 import de.unisaarland.cs.sopra.common.model.ResourcePackage;
@@ -16,14 +14,12 @@ public class CatapultTest {
 	Catapult c;
 	
 	public void testPrice(){
-		pl=new Player();
-		c=new Catapult(pl);
 		ResourcePackage buildingPrice = new ResourcePackage(-1,0,-1,0,-1);
 		ResourcePackage attackCatapultPrice = new ResourcePackage(0,0,0,-1,0);
 		ResourcePackage attackBuildingPrice = new ResourcePackage(0,0,0,0,-1);
-		assertEquals(buildingPrice,c.getBuildingprice());
-		assertEquals(attackBuildingPrice,c.getAttackbuildingprice());
-		assertEquals(attackCatapultPrice,c.getAttackcatapultprice());
+		assertEquals(buildingPrice,Catapult.getBuildingprice());
+		assertEquals(attackBuildingPrice,Catapult.getAttackbuildingprice());
+		assertEquals(attackCatapultPrice,Catapult.getAttackcatapultprice());
 	}
 	
 	public void testOwner(){
