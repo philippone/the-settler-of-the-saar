a275b14cce1605891e91f8a321d73ad749fc3275
Philipp Schardt <philipp@stud.uni-saarland.de>
2011-09-05 11:23:10 +0200

    implementation removed

--- implementation/saarsiedler-group16/src/main/java/de/unisaarland/cs/sopra/common/view/MoveCatapult.java
+++
@@ -1,9 +0,0 @@
-package de.unisaarland.cs.sopra.common.view;
-
-public class MoveCatapult implements Strategy{
-
-	public void execute() {
-		throw new UnsupportedOperationException();
-	}
-	
-}
--- implementation/saarsiedler-group16/src/test/java/model/BuildingTest.java
+++
@@ -1,9 +0,0 @@
-package model;
-
-import static org.junit.Assert.*;
-import org.junit.Test;
-import de.unisaarland.cs.sopra.common.model.Building;
-
-public class BuildingTest {
-	
-}
