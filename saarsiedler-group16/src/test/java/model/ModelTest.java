5dcae8140477a099206b3b56be73108ae4d702c4
Philipp Schardt <philipp@stud.uni-saarland.de>
2011-09-05 14:49:23 +0200

    ModelTest: tests ohne Rumpf...

--- saarsiedler-group16/src/test/java/model/ModelTest.java
+++ saarsiedler-group16/src/test/java/model/ModelTest.java
@@ -1,5 +1,77 @@
 package model;
 
+import static org.junit.Assert.*;
+import help.TestUtil;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import de.unisaarland.cs.sopra.common.model.Model;
+
 public class ModelTest {
+	
+	private Model model;
+	
+	@Before
+	public void setUp() {
+		model = TestUtil.getStandardModel();
+	}
+	
+	@Test
+	public void testAddObserver() {
+		
+	}
+	
+	
+	@Test
+	public void testRemoveObserver() {
+		
+	}
+	
+	@Test
+	public void testGetCurrentPlayer() {
+		
+	}
+	
+	@Test
+	public void testCalculateLongestRoad() {
+		
+	}
+	
+	@Test
+	public void setTableOrder() {
+		
+	}
+	
+	@Test
+	public void setFieldNumbers() {
+		
+	}
+	
+	@Test
+	public void updateLongestRoad() {
+		
+	}
+	
+	@Test
+	public void getLocationField() {
+		
+	}
+	
+	@Test
+	public void getLocationIntersection() {
+		
+	}
+	
+	@Test
+	public void getLocationPath() {
+		
+	}
+	
+	
+	
+	
+	
+	
 
 }
