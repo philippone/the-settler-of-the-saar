47ee5c8559d8d324e12eb9be9b0123df17d98e54
Philipp Schardt <philipp@stud.uni-saarland.de>
2011-09-05 12:48:13 +0200

    ControllerTest und TestUtil

--- saarsiedler-group16/src/test/java/controller/ControllerTest.java
+++ saarsiedler-group16/src/test/java/controller/ControllerTest.java
@@ -1,12 +1,28 @@
 package controller;
 
 import static org.junit.Assert.*;
+import help.TestUtil;
+
 import org.junit.Before;
 import org.junit.Test;
 
+import de.unisaarland.cs.sopra.common.model.ModelWriter;
+import de.unisaarland.cs.st.saarsiedler.comm.Connection;
 
 public class ControllerTest {
 	
+	private Connection connection;
+	private ModelWriter modelWriter;
 	
+	
+	@Before
+	public void setUp() {
+		connection = TestUtil.getTestConnection();
+		modelWriter = TestUtil.getTestModel();
+	}
+	
+	public void testAttack() {
+		
+	}
 
 }
---
+++ saarsiedler-group16/src/test/java/help/TestUtil.java
@@ -0,0 +1,17 @@
+package help;
+
+import de.unisaarland.cs.sopra.common.model.Model;
+import de.unisaarland.cs.st.saarsiedler.comm.Connection;
+
+
+public class TestUtil {
+	
+	public static Model getTestModel() {
+		return null;
+	}
+	
+	public static Connection getTestConnection() {
+		return null;
+	}
+
+}
