0a586f83cf4453cd37c06a1fcdf4db7887e4e3c9
Patrick Bender <ichbinkeinreh@t-online.de>
2011-09-09 00:27:00 +0200

    genug fuer heute geschafft

--- saarsiedler-group16/src/main/java/de/unisaarland/cs/sopra/common/view/GameGUI.java
+++ saarsiedler-group16/src/main/java/de/unisaarland/cs/sopra/common/view/GameGUI.java
@@ -3,8 +3,11 @@ package de.unisaarland.cs.sopra.common.view;
 import java.io.FileInputStream;
 import java.util.HashMap;
 import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Map;
 
+import org.lwjgl.LWJGLException;
 import org.lwjgl.opengl.Display;
 import org.lwjgl.opengl.DisplayMode;
 import org.lwjgl.opengl.GL11;
@@ -29,9 +32,10 @@ import de.unisaarland.cs.st.saarsiedler.comm.WorldRepresentation;
 public class GameGUI extends View implements Runnable{
 
 	private Map<Player,String> playerNames;
-	private int angle;
 	private Setting setting;
 	private Map<FieldType,Texture> textureMap;
+	private Map<Integer,Texture> numberTextureMap;
+	private List<Field> renderFieldList;
 	
 	GameGUI(long meID, ModelReader modelReader, ControllerAdapter controllerAdapter, Map<Player,String> playerNames, Setting setting) throws Exception {
 		super(meID, modelReader, controllerAdapter);
@@ -39,33 +43,23 @@ public class GameGUI extends View implements Runnable{
 		this.setting = setting;
 		this.playerNames = playerNames;
 		this.controllerAdapter = controllerAdapter;
-		
-		Display.setDisplayMode(new DisplayMode(setting.getWindowWidth(), setting.getWindowHeight()));
-		Display.setFullscreen(setting.isFullscreen());
-		Display.setTitle("Die Siedler von der Saar");
-		Display.setVSyncEnabled(true);
-		Display.create();
-		
-		textureMap = new HashMap<FieldType,Texture>();
-		textureMap.put(FieldType.MOUNTAINS, TextureLoader.getTexture("JPG", new FileInputStream("src/main/resources/Mountain.png")));
-		textureMap.put(FieldType.DESERT, TextureLoader.getTexture("JPG", new FileInputStream("src/main/resources/Mountain.png")));
-		textureMap.put(FieldType.FIELDS, TextureLoader.getTexture("JPG", new FileInputStream("src/main/resources/Mountain.png")));
-		textureMap.put(FieldType.FOREST, TextureLoader.getTexture("JPG", new FileInputStream("src/main/resources/Mountain.png")));
-		textureMap.put(FieldType.HILLS, TextureLoader.getTexture("JPG", new FileInputStream("src/main/resources/Mountain.png")));
-		textureMap.put(FieldType.PASTURE, TextureLoader.getTexture("JPG", new FileInputStream("src/main/resources/Mountain.png")));
-		textureMap.put(FieldType.WATER, TextureLoader.getTexture("JPG", new FileInputStream("src/main/resources/Mountain.png")));
-		
-		GL11.glDisable(GL11.GL_DEPTH_TEST); // Enables Depth Testing
-		GL11.glEnable(GL11.GL_TEXTURE_2D);
-        GL11.glTexParameteri(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_MIN_FILTER, GL11.GL_LINEAR);
-        GL11.glTexParameteri(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_MAG_FILTER, GL11.GL_LINEAR);
-        GL11.glClearDepth(1.0f); // Depth Buffer Setup
-        GL11.glEnable(GL11.GL_BLEND);
-        GL11.glBlendFunc(GL11.GL_SRC_ALPHA,GL11.GL_ONE);
 	}
 	
-	private void renderField(int x, int y, FieldType fieldType) {
-	     textureMap.get(fieldType).bind();
+	private void renderField(Field f) {
+		int x = 0;
+		int y = 0;
+		   switch(f.getLocation().getY()%2) {
+		   case 0:
+			   x = f.getLocation().getX()*250;
+			   y = f.getLocation().getY()*-215; 
+			   break;
+		   case 1:
+			   x = f.getLocation().getX()*250-125;
+			   y = f.getLocation().getY()*-215;
+			   break;
+		   }
+		
+	     textureMap.get(f.getFieldType()).bind();
 	     GL11.glBegin(GL11.GL_POLYGON);
 	      // GL11.glColor4f(1.0f,1.0f,1.0f,1.0f); transparenz
 	       GL11.glTexCoord2f(0,0);
@@ -79,33 +73,30 @@ public class GameGUI extends View implements Runnable{
 	     GL11.glEnd();
 	}
 	
+	
+	
 	private void render() {
-
 		   GL11.glMatrixMode(GL11.GL_PROJECTION);
 		   GL11.glLoadIdentity();
-		   GLU.gluPerspective(45.0f, 1280.0f/1024.0f, 0.1f, 5000.0f); //-5000.f ist die maximale z tiefe
+		   GLU.gluPerspective(45.0f, ((float)setting.getWindowWidth())/setting.getWindowHeight(), 0.1f, 5000.0f); //-5000.f ist die maximale z tiefe
 		   GL11.glMatrixMode(GL11.GL_MODELVIEW);
 		   // clear the screen
-		   GL11.glClear(GL11.GL_COLOR_BUFFER_BIT | GL11.GL_DEPTH_BUFFER_BIT);
+//		   GL11.glClear(GL11.GL_COLOR_BUFFER_BIT | GL11.GL_DEPTH_BUFFER_BIT);
 		   // center square according to screen size
 		   GL11.glPushMatrix();
-		   GL11.glTranslatef(0.0f, -0.0f, angle--); //-4000 ist z koord
+		   GL11.glTranslatef(0.0f, -0.0f, -2000.0f); //-4000 ist z koord
 			// rotate square according to angle
-		   GL11.glRotatef(/*angle++*/0, 0, 0, 1.0f);
+		   GL11.glRotatef(0, 0, 0, 0);
 		   GL11.glColor3f(1.0f, 1.0f, 1.0f);
+		   //TODO: center map and calculate initial zoom!!!
 	
 		  // GL11.glTranslatef(-modelReader.getBoardWidth()*215/2, modelReader.getBoardHeight()*250/2, 0.0f);
 		   
-		   Iterator<Field> iter = modelReader.getFieldIterator();
+		   Iterator<Field> iter = renderFieldList.iterator();
 		   while (iter.hasNext()) {
-			   Field f = iter.next();
-			   switch(f.getLocation().getY()%2) {
-			   case 0: renderField(f.getLocation().getX()*250, f.getLocation().getY()*215, f.getFieldType()); break;
-			   case 1: renderField(f.getLocation().getX()*250+125, f.getLocation().getY()*215, f.getFieldType()); break;
+			   		renderField(iter.next());
+			   		//iter.remove();
 			   }
-
-		   }
-
 		   GL11.glPopMatrix();
 	}
 
@@ -186,6 +177,77 @@ public class GameGUI extends View implements Runnable{
 		throw new UnsupportedOperationException();
 	}
 	
+	@Override
+	public void run() {
+		
+		try {
+			Display.setDisplayMode(new DisplayMode(setting.getWindowWidth(), setting.getWindowHeight()));
+			Display.setFullscreen(setting.isFullscreen());
+			Display.setTitle("Die Siedler von der Saar");
+			Display.setVSyncEnabled(true);
+			Display.create();
+			
+			textureMap = new HashMap<FieldType,Texture>();
+			textureMap.put(FieldType.MOUNTAINS, TextureLoader.getTexture("JPG", new FileInputStream("src/main/resources/Textures/Mountains.png")));
+			textureMap.put(FieldType.DESERT, TextureLoader.getTexture("JPG", new FileInputStream("src/main/resources/Textures/Desert.png")));
+			textureMap.put(FieldType.FIELDS, TextureLoader.getTexture("JPG", new FileInputStream("src/main/resources/Textures/Fields.png")));
+			textureMap.put(FieldType.FOREST, TextureLoader.getTexture("JPG", new FileInputStream("src/main/resources/Textures/Forest.png")));
+			textureMap.put(FieldType.HILLS, TextureLoader.getTexture("JPG", new FileInputStream("src/main/resources/Textures/Hills.png")));
+			textureMap.put(FieldType.PASTURE, TextureLoader.getTexture("JPG", new FileInputStream("src/main/resources/Textures/Pasture.png")));
+			textureMap.put(FieldType.WATER, TextureLoader.getTexture("JPG", new FileInputStream("src/main/resources/Textures/Water.png")));
+			
+			numberTextureMap = new HashMap<Integer,Texture>();
+			numberTextureMap.put(5, TextureLoader.getTexture("JPG", new FileInputStream("src/main/resources/Textures/5.png")));
+		} catch (Exception e) {}
+		
+		GL11.glDisable(GL11.GL_DEPTH_TEST); // Enables Depth Testing
+		GL11.glEnable(GL11.GL_TEXTURE_2D);
+        GL11.glTexParameteri(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_MIN_FILTER, GL11.GL_LINEAR);
+        GL11.glTexParameteri(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_MAG_FILTER, GL11.GL_LINEAR);
+        GL11.glClearDepth(1.0f); // Depth Buffer Setup
+        GL11.glEnable(GL11.GL_BLEND);
+        GL11.glBlendFunc(GL11.GL_SRC_ALPHA,GL11.GL_ONE);
+		
+        renderFieldList = new LinkedList<Field>();
+        Iterator<Field> iter = modelReader.getFieldIterator();
+        while (iter.hasNext()) {
+        	renderFieldList.add(iter.next());
+        }
+        
+        Display.update();
+        render();
+        Display.update();
+        render();
+        //render two times to fill the double buffer
+        //TODO: how to handle zoom and scroll?
+
+    boolean finished = false;
+		while (!finished) {
+		      // Always call Window.update(), all the time - it does some behind the
+		      // scenes work, and also displays the rendered output
+			  Display.update();
+		      // Check for close requests
+		      if (Display.isCloseRequested()) {
+			  finished = true;
+		      } 
+		      else if (Display.isActive()) {
+		          //render();
+		          //Display.sync(60);
+		        } 
+		      // The window is not in the foreground, so we can allow other stuff to run and
+		      // infrequently update
+		      else {
+		        try {
+		          Thread.sleep(100);
+		        } catch (InterruptedException e) {}
+		      }
+		      if (Display.isVisible() || Display.isDirty()) {
+		          //render();
+		      }
+		}
+		Display.destroy();
+	}
+	
 	public static void main(String[] args) throws Exception {
 		WorldRepresentation worldrep = new WorldRepresentation(4, 4, 2, 9, 5, 4,  
 				new byte[] {1,2,3,4,
@@ -241,44 +303,13 @@ public class GameGUI extends View implements Runnable{
 			}
 		};
 		Model model = new Model(worldrep, matchinfo, 0);
-		model.matchStart(new long[] {0,1}, new byte[] {2,3,4,5,
+		model.matchStart(new long[] {0,1}, new byte[]   {2,3,4,5,
 														 6,8,9,10,
 														 11,12,11,10,
 														 9,8,6,5});
-		Setting setting = new Setting(1280, 1024, false);
+		Setting setting = new Setting(1024, 600, false);
 		GameGUI gameGUI = new GameGUI(0, model, null, null, setting);
 		new Thread(gameGUI).start();
-	}
-
-	@Override
-	public void run() {
-    boolean finished = false;
-	    
-		while (!finished) {
-		      // Always call Window.update(), all the time - it does some behind the
-		      // scenes work, and also displays the rendered output
-		      Display.update();
-		 
-		      // Check for close requests
-		      if (Display.isCloseRequested()) {
-			  finished = true;
-		      } 
-		      else if (Display.isActive()) {
-		          render();
-		          Display.sync(60);
-		        } 
-		      // The window is not in the foreground, so we can allow other stuff to run and
-		      // infrequently update
-		      else {
-		        try {
-		          Thread.sleep(100);
-		        } catch (InterruptedException e) {}
-		      }
-		      if (Display.isVisible() || Display.isDirty()) {
-		          render();
-		      }
-		}
-		Display.destroy();
 	}
 	
 }
