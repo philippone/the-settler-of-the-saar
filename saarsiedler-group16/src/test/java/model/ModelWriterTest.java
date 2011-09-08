51cac52e322fafdf8694ee4c2ff04534615a11aa
Kai Glauber <square.the@googlemail.com>
2011-09-08 09:29:42 +0200

    Fixed Errors in ModelWriterTest

--- saarsiedler-group16/src/test/java/model/ModelWriterTest.java
+++ saarsiedler-group16/src/test/java/model/ModelWriterTest.java
@@ -7,6 +7,7 @@ import static org.junit.Assert.fail;
 import help.TestUtil;
 
 import java.io.IOException;
+import java.util.Set;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -30,21 +31,20 @@ public class ModelWriterTest {
 		model.getTableOrder().get(0).modifyResources(new ResourcePackage(-100,-100,-100,-100,-100));
 	}
 	
-	public void initalize(){
+	public void initialize(){
 		//Build inital stuff
 				model.buildSettlement(new Location(1, 1, 0), BuildingType.Village);
 				model.buildStreet(new Location(1, 1, 0));
 				model.buildSettlement(new Location(2, 1, 3), BuildingType.Village);
 				model.buildStreet(new Location(2, 1, 3));
 				model.newRound(12);
-				model.getPlayerMap().get(id)
 	}
 	
 	@Test
 	// Tests wheter you are able to place a catapult next to a village (!= town).
 	// An IllegalStateException should be thrown in that case.
 	public void buildCatapultTest() {
-		initalize();
+		initialize();
 		try {
 			model.buildCatapult(new Location(1, 1, 0), true);
 			fail("You was able to build a catapult close to a village");
@@ -55,7 +55,7 @@ public class ModelWriterTest {
 	@Test
 	// Tests wheter your catapult was successfully builded and that the owner is the right one.
 	public void buildCatapult2Test() {
-		initalize();
+		initialize();
 		Player currentPlayer = model.getCurrentPlayer();
 		model.buildSettlement(new Location(1, 1, 0), BuildingType.Town);
 		model.buildCatapult(new Location(1, 1, 0), true);
@@ -67,7 +67,7 @@ public class ModelWriterTest {
 	@Test
 	// Tests wheter your catapult wasn't successfully builded and that the owner is the right one.
 	public void buildCatapult2_negativeTest() {
-		initalize();
+		initialize();
 		Player currentPlayer = model.getCurrentPlayer();
 		model.buildSettlement(new Location(1, 1, 0), BuildingType.Town);
 		model.buildCatapult(new Location(1, 1, 0), false);
@@ -79,7 +79,7 @@ public class ModelWriterTest {
 	@Test
 	// Tests wheter the currentPlayer has enough resources to build a catapult.
 	public void buildCatapult3_buildTest() {
-		initalize();
+		initialize();
 		model.newRound(2);
 		try {
 			model.buildCatapult(new Location(2, 1, 3), false);
@@ -90,7 +90,7 @@ public class ModelWriterTest {
 	@Test
 	// Tests wheter the currentPlayer has enough resources to move a catapult.
 	public void buildCatapult3_moveTest() {
-		initalize();
+		initialize();
 		model.newRound(2);
 		model.getCurrentPlayer().modifyResources(new ResourcePackage(1000, 1000, 1000, 1000, 1000));
 		model.buildCatapult(new Location(2, 1, 3), false);
@@ -105,7 +105,7 @@ public class ModelWriterTest {
 	// Tests wheter an exception is thrown when the fightoutcome is false, but there is no catapult that
 	// could have destroyed the attacking catapult
 	public void catapulCatapult4_buildCatapultExceptionTest() {
-		initalize();
+		initialize();
 		try {
 			model.buildCatapult(new Location(1, 1, 0), false);
 			fail("You lost against a not existing catapult!");
@@ -116,7 +116,7 @@ public class ModelWriterTest {
 	@Test
 	// Tests wheter the currentPlayer has enough resources to attack a settlement.
 	public void buildCatapult3_attackSettlementTest() {
-		initalize();
+		initialize();
 		model.newRound(2);
 		Path newCatPath = model.getPath(new Location(2, 1, 4));
 		newCatPath.createCatapult(model.getCurrentPlayer());
@@ -130,7 +130,7 @@ public class ModelWriterTest {
 	@Test
 	// Tests wheter it is possible to build more then the maximum number of catapults
 	public void buildCatapult4_maxTest() {
-		initalize();
+		initialize();
 		Player currentPlayer = model.getCurrentPlayer();
 		Path path1 = model.getPath(new Location(1, 1, 0));
 		path1.createCatapult(currentPlayer);
@@ -160,7 +160,7 @@ public class ModelWriterTest {
 	@Test
 	// Test wheter we have got enough resources to build the specified Settlement
 	public void buildSettlement_costpositiveTest() {
-		initalize();
+		initialize();
 		model.buildStreet(new Location(1, 1, 1));
 		try {
 			model.buildSettlement(new Location(1, 1, 2), BuildingType.Village);
@@ -172,7 +172,7 @@ public class ModelWriterTest {
 	@Test
 	// Tests wheter we have not got enough resources to build the specified Settlement
 	public void buildSettlement_costnegativeTest() {
-		initalize();
+		initialize();
 		model.newRound(2);
 		model.getPath(new Location(2, 2, 3)).createStreet(model.getCurrentPlayer());
 		try {
@@ -185,7 +185,7 @@ public class ModelWriterTest {
 	@Test
 	// Tests wheter we can build a building although there is a street missing.
 	public void buildSettlement_missingStreetTest(){
-		initalize();
+		initialize();
 		try {
 			model.buildSettlement(new Location(2, 2, 0), BuildingType.Village);
 			fail("There is no steet that allows you to build a new Village!");
@@ -197,7 +197,7 @@ public class ModelWriterTest {
 	// Tests wheter there is another base to close to the intersection on which you
 	// wish to place your new village
 	public void buildSettlement_toCloseTest() {
-		initalize();
+		initialize();
 		Player currentPlayer = model.getCurrentPlayer();
 		model.getPath(new Location(1, 1, 1)).createStreet(currentPlayer);
 		model.getPath(new Location(2, 2, 0)).createStreet(currentPlayer);
@@ -213,7 +213,7 @@ public class ModelWriterTest {
 	@Test
 	// Tests wheter there is already a settlement on the destination intersection
 	public void buildSettlement_alreadyBuiltTest() {
-		initalize();
+		initialize();
 		Player currentPlayer = model.getCurrentPlayer();
 		model.getPath(new Location(1, 1, 1)).createStreet(currentPlayer);
 		model.getPath(new Location(2, 2, 0)).createStreet(currentPlayer);
@@ -229,7 +229,7 @@ public class ModelWriterTest {
 	@Test
 	// Tests wheter you are able to upgrade your village to a town
 	public void buildSettlement_upgradePositiveTest(){
-		initalize();
+		initialize();
 		model.getIntersection(new Location(1, 1, 3)).createBuilding(BuildingType.Village, model.getCurrentPlayer());
 		try {
 			model.buildSettlement(new Location(1, 1, 3), BuildingType.Town);
@@ -243,16 +243,16 @@ public class ModelWriterTest {
 	@Test
 	// Tests wheter you are able to upgrade your village to a town
 	public void buildSettlement_upgradeNegativeTest(){
-		initalize();
+		initialize();
 		Player currentPlayer = model.getCurrentPlayer();
 		model.getPath(new Location(1, 1, 1)).createStreet(currentPlayer);
-		model.getPath(new Location())
+		model.getPath(new Location(1, 1, 2)).createStreet(currentPlayer);
 		try {
 			model.buildSettlement(new Location(1, 1, 3), BuildingType.Town);
-			// everything is fine
+			fail("You shouldn't be able to upgrade your town!");
 		}
 		catch (IllegalStateException e){
-			fail("You should be able to upgrade your town!");
+			// everything is fine
 		}
 	}
 	
@@ -312,6 +312,4 @@ public class ModelWriterTest {
 		assertEquals(new ResourcePackage(2, 5, 6, 0, 1), p.getResources());
 	}
 	
-}
-
 }
