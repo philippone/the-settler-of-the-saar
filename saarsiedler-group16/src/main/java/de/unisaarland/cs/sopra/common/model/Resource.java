278522b63a9edc0c501de5f7b8e482f3a1dc325a
Patrick Bender <ichbinkeinreh@t-online.de>
2011-09-08 13:17:52 +0200

    !!!!!!!!!!!!

--- saarsiedler-group16/src/main/java/de/unisaarland/cs/sopra/common/model/Resource.java
+++ saarsiedler-group16/src/main/java/de/unisaarland/cs/sopra/common/model/Resource.java
@@ -1,5 +1,26 @@
 package de.unisaarland.cs.sopra.common.model;
 
+
 public enum Resource {
-	LUMBER, BRICK, WOOL, GRAIN, ORE
+	LUMBER, BRICK, WOOL, GRAIN, ORE;
+
+	public static Resource convert (de.unisaarland.cs.st.saarsiedler.comm.Resource resource) {
+		switch(resource) {
+			case Lumber:
+				return LUMBER;
+			case Brick:
+				return BRICK;
+			case Grain:
+				return GRAIN;
+			case Ore:
+				return ORE;
+			case Wool:
+				return WOOL;
+			default: 
+				throw new IllegalStateException();
+		}
+	}
+
 }
+
+
