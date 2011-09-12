364ca45f48464224a99f071803c7ec31499dca00
Kai Glauber <square.the@googlemail.com>
2011-09-12 12:34:00 +0200

    BOAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAARRRRRRRRDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDD!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!

--- saarsiedler-group16/src/test/java/model/BoardTest.java
+++ saarsiedler-group16/src/test/java/model/BoardTest.java
@@ -192,7 +192,7 @@ public class BoardTest {
 		//create the neighbours of the intersection
 		path9 = b.getPath(new Location(0, 0, 1));
 		path10 = b.getPath(new Location(0, 0, 2));
-		path11 = b.getPath(new Location(0, 1, 4));
+		path11 = b.getPath(new Location(0, 1, 3));
 		//create both Sets
 		pathSet3 = b.getPathsFromIntersection(i3);
 		pathSet4 = new HashSet<Path>();
