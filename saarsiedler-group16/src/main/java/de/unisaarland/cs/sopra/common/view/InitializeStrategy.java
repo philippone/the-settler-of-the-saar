46232820caaf66e06b18b13191047e4b45c769d8
Elitsa Kaloyanova <elitza.kaloyanova@gmail.com>
2011-09-14 10:24:46 +0200

    initstrategyclass

---
+++ saarsiedler-group16/src/main/java/de/unisaarland/cs/sopra/common/view/InitializeStrategy.java
@@ -0,0 +1,17 @@
+package de.unisaarland.cs.sopra.common.view;
+
+import de.unisaarland.cs.sopra.common.controller.ControllerAdapter;
+import de.unisaarland.cs.sopra.common.model.ModelReader;
+
+public class InitializeStrategy implements Strategy {
+
+	@Override
+	public void execute(ModelReader mr, ControllerAdapter ca) {
+		
+
+	}
+	public int evaluate(){
+		// TODO implement this method
+		return 0;
+	}
+}
