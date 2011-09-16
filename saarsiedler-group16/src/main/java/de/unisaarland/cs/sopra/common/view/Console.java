e3978a83e31b571c976e0d2f022d6f696e41fb48
Patrick Bender <ichbinkeinreh@t-online.de>
2011-09-16 17:42:27 +0200

    added console

---
+++ saarsiedler-group16/src/main/java/de/unisaarland/cs/sopra/common/view/Console.java
@@ -0,0 +1,28 @@
+package de.unisaarland.cs.sopra.common.view;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Console {
+	
+	private List<String> text;
+	private int index;
+
+	public Console() {
+		this.index = 0;
+		this.text = new ArrayList<String>();
+	}
+	
+	public void addText (String text) {
+		//TODO: implement it!
+	}
+	
+	public void scrollDown() {
+		//TODO: implement it!
+	}
+	
+	public void scrollUp() {
+		//TODO: implement it!
+	}
+	
+}
