618484ae05edc544b058a4338eed32fc9c63192e
Kai Glauber <square.the@googlemail.com>
2011-09-20 14:15:25 +0200

    the same as before. just forgett to add the new stuff

---
+++ saarsiedler-group16/src/main/java/de/unisaarland/cs/sopra/common/view/ai/ReturnResourcesStrategy.java
@@ -0,0 +1,57 @@
+package de.unisaarland.cs.sopra.common.view.ai;
+
+import de.unisaarland.cs.sopra.common.model.ModelReader;
+
+public class ReturnResourcesStrategy extends Strategy {
+	
+	public ReturnResourcesStrategy(ModelReader mr){
+		super(mr);
+	}
+
+	@Override
+	public double evaluate(AttackCatapult stroke) {
+		return 0;
+	}
+
+	@Override
+	public double evaluate(AttackSettlement stroke) {
+		return 0;
+	}
+
+	@Override
+	public double evaluate(BuildVillage stroke) {
+		return 0;
+	}
+
+	@Override
+	public double evaluate(BuildTown stroke) {
+		return 0;
+	}
+
+	@Override
+	public double evaluate(BuildCatapult stroke) {
+		return 0;
+	}
+
+	@Override
+	public double evaluate(BuildStreet stroke) {
+		return 0;
+	}
+
+	@Override
+	public double evaluate(MoveCatapult stroke) {
+		return 0;
+	}
+
+	@Override
+	public double evaluate(MoveRobber stroke) {
+		return 0;
+	}
+
+	@Override
+	public double evaluate(ReturnResources stroke) {
+		// TODO Implement this Method
+		return 0;
+	}
+
+}
