5b74e88d5ea3a14b4f3dba53d8c2fe428784e9f2
Kai Glauber <square.the@googlemail.com>
2011-09-21 11:43:39 +0200

    lksafj

--- saarsiedler-group16/src/main/java/de/unisaarland/cs/sopra/common/view/ai/GreedyTradeOfferStrategy.java
+++ saarsiedler-group16/src/main/java/de/unisaarland/cs/sopra/common/view/ai/GreedyTradeOfferStrategy.java
@@ -1,6 +1,7 @@
 package de.unisaarland.cs.sopra.common.view.ai;
 
 import de.unisaarland.cs.sopra.common.controller.ControllerAdapter;
+import de.unisaarland.cs.sopra.common.model.Resource;
 import de.unisaarland.cs.sopra.common.model.ResourcePackage;
 
 public class GreedyTradeOfferStrategy extends TradeOfferStrategy {
@@ -10,7 +11,11 @@ public class GreedyTradeOfferStrategy extends TradeOfferStrategy {
 	}
 	
 	public void execute() {
-		
+		do {
+			Resource maxR = getMaxResource(possessions);
+			Resource minR = getMinResource(price);
+			
+		} while (possessions.add(price).hasNegativeResources());
 	}
 
 }
