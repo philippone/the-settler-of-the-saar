fdf052540ac18d046fcb7fe6e7c708404ccf3b71
Philipp Schardt <philipp@stud.uni-saarland.de>
2011-09-23 14:06:05 +0200

    kp

--- implementation/saarsiedler-group16/src/main/java/de/unisaarland/cs/sopra/common/view/ai/Ai.java
+++ implementation/saarsiedler-group16/src/main/java/de/unisaarland/cs/sopra/common/view/ai/Ai.java
@@ -35,7 +35,7 @@ public class Ai implements ModelObserver {
 	private final Set<Strategy> returnResourcesStrategies;
 	private final Set<Strategy> initStrategies;
 	
-	public Ai(ModelReader mr, ControllerAdapter ca){
+	public Ai(ModelReader mr, ControllerAdapter ca){ 
 		this.mr = mr;
 		this.ca = ca;
 		this.generalStrategies = new HashSet<Strategy>();
