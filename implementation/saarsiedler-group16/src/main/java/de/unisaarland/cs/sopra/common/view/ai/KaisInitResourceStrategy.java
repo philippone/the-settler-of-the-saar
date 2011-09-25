6b2b972918a043e73a32f64826be9c08f4e73d2f
Kai Glauber <square.the@googlemail.com>
2011-09-25 17:05:40 +0200

    Wo ist der abgewandelte execute loop hin?? und wieso wurde mein kram komplett gelöscht anstatt ihn einfach auszukommentieren?

---
+++ implementation/saarsiedler-group16/src/main/java/de/unisaarland/cs/sopra/common/view/ai/KaisInitResourceStrategy.java
@@ -0,0 +1,143 @@
+package de.unisaarland.cs.sopra.common.view.ai;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import de.unisaarland.cs.sopra.common.model.BuildingType;
+import de.unisaarland.cs.sopra.common.model.Field;
+import de.unisaarland.cs.sopra.common.model.Intersection;
+import de.unisaarland.cs.sopra.common.model.ModelReader;
+import de.unisaarland.cs.sopra.common.model.Resource;
+
+public class KaisInitResourceStrategy extends Strategy {
+	
+
+	public KaisInitResourceStrategy(ModelReader mr) {
+		super(mr);
+	}
+
+	@Override
+	public boolean evaluates(Stroke s){
+		switch(s.getType()){
+		default:
+			throw new IllegalArgumentException("The stroke is no valid stroke!");
+		case ATTACK_CATAPULT:
+			return false;
+		case ATTACK_SETTLEMENT:
+			return false;
+		case BUILD_VILLAGE:
+			return true;
+		case BUILD_TOWN:
+			return false;
+		case BUILD_CATAPULT:
+			return false;
+		case BUILD_STREET:
+			return false;
+		case MOVE_CATAPULT:
+			return false;
+		case MOVE_ROBBER:
+			return false;
+		case RETURN_RESOURCES:
+			return false;
+		}
+	}
+
+	@Override
+	public double importance() {
+		return 1;
+	}
+
+	@Override
+	public double evaluate(AttackCatapult stroke) {
+		return 0;
+	}
+
+	@Override
+	public double evaluate(AttackSettlement stroke) {
+		return 0;
+	}
+
+	@Override
+	public double evaluate(BuildVillage stroke) {
+		double evaluation = 0;
+		Intersection destination = stroke.getDestination();
+		Set<Resource> reslist = new HashSet<Resource>();
+		// try to get 3 different resources
+		if (mr.getSettlements(mr.getMe(), BuildingType.Village).size() == 0){
+			getResourcesFromIntersection(destination);
+			if (reslist.size() < 3) evaluation = 0;
+			else evaluation = 1;
+		}
+		// try to get the missing ones to build a village
+		else {
+			int differentResources = 0;
+			for (Intersection otherVillages : mr.getSettlements(mr.getMe(), BuildingType.Village)){
+				reslist.addAll(getResourcesFromIntersection(otherVillages));
+			}
+			getMissingVillageResources(reslist);
+			for (Field neighbour : mr.getFieldsFromIntersection(destination)){
+				if (neighbour.getResource() != null){
+					if (!reslist.contains(neighbour.getResource())){
+						differentResources++;
+					}
+				}
+			}
+			if (reslist.size() + differentResources == 5) return 1;
+			else return 0;
+		}
+		return evaluation;
+	}
+	
+	private Set<Resource> getResourcesFromIntersection(Intersection inter){
+		Set<Resource> reslist = new HashSet<Resource>();
+		for (Field neighbour : mr.getFieldsFromIntersection(inter)){
+			if (neighbour.getResource() != null){
+				reslist.add(neighbour.getResource());
+			}
+		}
+		return reslist;
+	}
+	
+	private Set<Resource> getMissingVillageResources(Set<Resource> resourcesThatWeOwn){
+		Set<Resource> reslist = new HashSet<Resource>();
+		for (Resource res : Resource.values()){
+			if (BuildingType.Village.getPrice().getResource(res) < 0){
+				if (!resourcesThatWeOwn.contains(res)){
+					reslist.add(res);
+				}
+			}
+		}
+		return reslist;
+	}
+
+	@Override
+	public double evaluate(BuildTown stroke) {
+		return 0;
+	}
+
+	@Override
+	public double evaluate(BuildCatapult stroke) {
+		return 0;
+	}
+
+	@Override
+	public double evaluate(BuildStreet stroke) {
+		return 0;
+	}
+
+	@Override
+	public double evaluate(MoveCatapult stroke) {
+		return 0;
+	}
+
+	@Override
+	public double evaluate(MoveRobber stroke) {
+		return 0;
+	}
+
+	@Override
+	public double evaluate(ReturnResources stroke) {
+		return 0;
+	}
+
+}
